26d3c5e655c6699fa3ed1348d76a383fe749358f
Omar <08068593@student.hhs.nl>
2018-02-21 16:15:04 +0000

    Updated HomeController.cs

--- MovieTime.Web/Controllers/HomeController.cs
+++ MovieTime.Web/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+//test
+
 namespace MovieTime.Web.Controllers
 {   
     public class HomeController : Controller
