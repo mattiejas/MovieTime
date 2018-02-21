cc4e3d223d572e3f04e80d526674f41e65b80506
Matthias Aarnoutse <mattiejas@outlook.com>
2018-02-21 13:08:38 +0100

    Remove Typescript from Webpack and fail to add SASS

--- MovieTime.Web/ClientApp/components/Layout.jsx
+++ MovieTime.Web/ClientApp/components/Layout.jsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { NavMenu } from './NavMenu';
 
-export class Layout extends React.Component<{}, {}> {
-    public render() {
+export class Layout extends React.Component {
+    render() {
         return <div className='container-fluid'>
             <div className='row'>
                 <div className='col-sm-3'>
---
+++ MovieTime.Web/ClientApp/views/Counter.jsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+
+import styles from './Counter.scss';
+
+export class Counter extends React.Component {
+    constructor() {
+        super();
+        this.state = {currentCount: 0};
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Counter</h1>
+
+                <p>This is a simple example of a React component.</p>
+
+                <p>Current count: <strong>{this.state.currentCount}</strong></p>
+
+                <button onClick={() => {
+                    this.incrementCounter()
+                }}>Increment
+                </button>
+                
+                <div className={styles.test}>
+                    Hello, world!
+                </div>
+            </div>
+        );
+    }
+
+    incrementCounter() {
+        this.setState({
+            currentCount: this.state.currentCount + 1
+        });
+    }
+}
