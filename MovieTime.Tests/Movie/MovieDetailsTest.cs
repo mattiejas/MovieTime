5ef4afc04be63b11416c77f540e66edd0faca327
Hermen Otter <hermenotter@outlook.com>
2018-02-22 16:18:55 +0100

    Add test for MovieController

--- MovieTime.Tests/Movie/MovieDetailsTest.cs
+++ MovieTime.Tests/Movie/MovieDetailsTest.cs
@@ -9,27 +9,33 @@ namespace MovieTime.Tests.Movie
         [Fact]
         public void GetMovieByIdTest()
         {
-            var movieModel = MovieService.GetMovieById("tt0112529");
+            var movieModel = new MovieService().GetMovieById("tt0112529");
             Assert.Equal("Blood Ring 2", movieModel.Title);
         }
         
         [Fact]
         public void GetMovieByTitleTest()
         {
-            var movieModel = MovieService.GetMovieByTitle("Blood Ring 2");
+            var movieModel = new MovieService().GetMovieByTitle("Blood Ring 2");
             Assert.Equal("tt0112529", movieModel.ImdbId);
         }
         
         [Fact]
         public void GetMoviesByTitleTest()
         {
-            var searchResultsModel = MovieService.GetMoviesByTitle("Ring");
+            var searchResultsModel = new MovieService().GetMoviesByTitle("Ring");
             // 635 results for Ring, can change in the future
             Assert.Equal(635, searchResultsModel.TotalResults);
             // 10 results on a page, can change in the future
             Assert.Equal(10, searchResultsModel.Movies.Count);
         }
         
-        
+        [Fact]
+        public void GetMoviesByTitleControllerTest()
+        {
+            var controller = new MovieController(new MovieService());
+            var searchResultsModel = controller.GetMovies("ring");
+            Assert.Equal(635, searchResultsModel.TotalResults);
+        }
     }
 }
