5a9e9518a656b730d26a8a858999bfe7d80b7eeb
Matthias Aarnoutse <mattiejas@outlook.com>
2018-02-22 15:05:27 +0100

    Create responsive navigation

--- MovieTime.Web/ClientApp/components/navigation/Navigation.jsx
+++ MovieTime.Web/ClientApp/components/navigation/Navigation.jsx
@@ -3,6 +3,7 @@ import classnames from 'classnames';
 
 import {Button} from '../button/Button';
 import styles from './Navigation.scss';
+import {NavLink} from "react-router-dom";
 
 export class Navigation extends Component {
   constructor() {
@@ -14,7 +15,6 @@ export class Navigation extends Component {
   }
 
   toggleMenu() {
-    console.log('opening menu...');
     this.setState({
       menuIsVisible: !this.state.menuIsVisible,
     });
@@ -23,11 +23,12 @@ export class Navigation extends Component {
   render() {
     return (
       <div className={classnames(styles.navigation)}>
-        <button className={styles.navButton} onClick={() => this.toggleMenu()}><i className="fa fa-bars" /></button>
-        <ul className={this.state.menuIsVisible ? '' : 'hidden'}>
-          <li>Home</li>
-          <li>Counter</li>
-          <li>Fetch Data</li>
+        <div className={styles.title}>Movie<span>Time</span></div>
+        <button onClick={() => this.toggleMenu()}><i className="fa fa-bars" /></button>
+        <ul className={this.state.menuIsVisible ? '' : styles['navigation--hidden']}>
+          <li><NavLink exact activeClassName={styles['navigation__item--active']} to="/">Home</NavLink></li>
+          <li><NavLink activeClassName={styles['navigation__item--active']} to="/some">Some</NavLink></li>
+          <li><NavLink activeClassName={styles['navigation__item--active']} to="/sample-data">Sample Data</NavLink></li>
         </ul>
       </div>
     );
