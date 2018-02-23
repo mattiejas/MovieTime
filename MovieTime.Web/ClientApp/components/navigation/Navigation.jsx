427636b2a4f6fa73e08881d0d4e5e53fd0b366fa
Matthias Aarnoutse <mattiejas@outlook.com>
2018-02-23 12:17:52 +0100

    Hide navigation for mobile on default

--- MovieTime.Web/ClientApp/components/navigation/Navigation.jsx
+++ MovieTime.Web/ClientApp/components/navigation/Navigation.jsx
@@ -11,7 +11,7 @@ export class Navigation extends Component {
     super();
 
     this.state = {
-      mobileMenuIsVisible: true,
+      mobileMenuIsVisible: false,
       inTransition: false,
     };
   }
