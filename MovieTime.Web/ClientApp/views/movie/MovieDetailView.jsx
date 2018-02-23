1db62768df270f77e4c5784695ed878a6a4ed78b
Jeffrey Dufour <jeffrey@nobody-here.wireless.hhs.nl>
2018-02-23 15:09:16 +0100

    Style MovieDetail component

--- MovieTime.Web/ClientApp/views/movie/MovieDetailView.jsx
+++ MovieTime.Web/ClientApp/views/movie/MovieDetailView.jsx
@@ -10,21 +10,34 @@ const MovieDetailView = () => (
   <div className={styles.view}>
     <div className={styles.view__background} />
     <div className={styles.view__content}>
-      <div className="container">
-        <MovieHeading title="Een super lange film" year="2017" />
-        <MoviePoster source="https://cdn.movieweb.com/img.teasers.posters/FIepTjimwgwohi_360_a/Thor.jpg" alt="Alt text" />
-        <MovieAttributes ranking="6" time="1h 2m 6s" genres="Action, Adventure, Comedy" />
-        <ul>
-          <li><b>Director:</b> Taika Waititi</li>
-          <li><b>Writers:</b> Eric Pearson, Craig Kyle</li>
-          <li><b>Actors:</b> Chris Hemsworth, Tom Hiddleston, Cate Blanchett</li>
-        </ul>
-        <p>
-          Thor is imprisoned on the other side of the universe and finds himself
-          in a race against time to get back to Asgard to stop Ragnarok,
-          the destruction of his homeworld and the end of Asgardian civilization,
-          at the hands of an all-powerful new threat, the ruthless Hela.
-        </p>
+      <div className={styles.view__content__container}>
+        <div className="*column">
+          <div className={styles.mobile}>
+            <MovieHeading title="Een super lange film" year="2017" />
+          </div>
+          <MoviePoster source="https://cdn.movieweb.com/img.teasers.posters/FIepTjimwgwohi_360_a/Thor.jpg" alt="Alt text" />
+        </div>
+        <div className="*column">
+          <div className={styles.view__content__heading}>
+            <div className="*flex-child">
+              <div className={styles.desktop}>
+                <MovieHeading title="Een super lange film" year="2017" />
+              </div>
+              <MovieAttributes ranking="6" time="1h 2m 6s" genres="Action, Adventure, Comedy" />
+            </div>
+          </div>
+          <ul>
+            <li><b>Director:</b> Taika Waititi</li>
+            <li><b>Writers:</b> Eric Pearson, Craig Kyle</li>
+            <li><b>Actors:</b> Chris Hemsworth, Tom Hiddleston, Cate Blanchett</li>
+          </ul>
+          <p>
+            Thor is imprisoned on the other side of the universe and finds himself
+            in a race against time to get back to Asgard to stop Ragnarok,
+            the destruction of his homeworld and the end of Asgardian civilization,
+            at the hands of an all-powerful new threat, the ruthless Hela.
+          </p>
+        </div>
       </div>
     </div>
   </div>
