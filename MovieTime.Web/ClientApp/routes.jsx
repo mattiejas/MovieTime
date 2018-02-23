a27c6ab502d24c217d768e550564dc13efb31553
Matthias Aarnoutse <mattiejas@outlook.com>
2018-02-23 15:02:42 +0100

    Add a 404 page

--- MovieTime.Web/ClientApp/routes.jsx
+++ MovieTime.Web/ClientApp/routes.jsx
@@ -1,14 +1,18 @@
 import * as React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Layout from './components/layout/Layout';
 import Home from './views/Home';
 import MovieDetailView from './views/movie/MovieDetailView';
+import NotFoundView from './views/notfound/NotFoundView';
 
 export const routes = (
   <Layout>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/movie/detail" component={MovieDetailView} />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/movie/detail" component={MovieDetailView} />
+      <Route component={NotFoundView} />
+    </Switch>
   </Layout>
 );
 
