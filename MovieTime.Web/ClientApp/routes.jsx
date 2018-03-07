b6160b6cac11cb828ba99b9aec51d6d813d0b286
Okkes <odoker@okkess-mbp.wireless.hhs.nl>
2018-03-07 10:10:14 +0100

    Init profile component

--- MovieTime.Web/ClientApp/routes.jsx
+++ MovieTime.Web/ClientApp/routes.jsx
@@ -1,19 +1,21 @@
 import * as React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 
 import Layout from './components/layout/Layout';
 import Home from './views/Home';
 import MovieDetailView from './views/movie/MovieDetailView';
+import ProfileView from './views/profile/ProfileView';
 import NotFoundView from './views/notfound/NotFoundView';
 
 export const routes = (
-  <Layout>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/movie/detail/:title" component={MovieDetailView} />
-      <Route component={NotFoundView} />
-    </Switch>
-  </Layout>
+    <Layout>
+        <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/movies/detail/:title" component={MovieDetailView}/>
+            <Route path="/users/:id" component={ProfileView}/>
+            <Route component={NotFoundView}/>
+        </Switch>
+    </Layout>
 );
 
 export default routes;
---
+++ MovieTime.Web/ClientApp/views/profile/ProfileView.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class ProfileView extends React.Component {
+  // reacts heeft comp, render zet je html
+  render() {
+    return <div>bla</div>;
+  }
+}
+
+export default ProfileView; // exporteert de module (naar iedereen die hier gebruik van maakt)
