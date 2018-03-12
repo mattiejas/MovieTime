4087cf32334d7ef84a91014acf8211f6558a913a
Matthias Aarnoutse <mattiejas@outlook.com>
2018-03-12 14:48:53 +0100

    Add production settings

--- MovieTime.Web/webpack.config.js
+++ MovieTime.Web/webpack.config.js
@@ -1,84 +1,122 @@
+require('babel-polyfill');
+
 const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = (env) => {
-    return {
-        entry: { main: ['babel-polyfill', './ClientApp/boot.jsx'] },
-        devtool: 'inline-source-map',
-        resolve: { extensions: ['.js', '.jsx'] },
-        module: {
-            rules: [
-                {
-                    enforce: "pre",
-                    test: /\.(js|jsx)$/,
-                    exclude: /node_modules/,
-                    loader: 'eslint-loader',
-                    options: {
-                        fix: true,
-                        emitWarning: true
-                    }
-                },
-                {
-                    test: /\.(js|jsx)$/,
-                    include: /ClientApp/,
-                    exclude: /(node_modules|bower_components)/,
-                    use: [
-                        {
-                            loader: 'babel-loader',
-                            options: {
-                                babelrc: false,
-                                presets: ['env', 'react'],
-                                plugins: ['transform-class-properties', 'transform-es2015-destructuring', 'transform-object-rest-spread', 'transform-async-to-generator'],
-                            },
-                        },
-                    ],
-                },
-                {
-                    test: /\.(png|jpg|jpeg|gif|svg)$/,
-                    include: /ClientApp/,
-                    use: 'url-loader?limit=25000',
-                },
-                {
-                    test: /\.scss$/,
-                    include: /ClientApp/,
-                    use: ExtractTextPlugin.extract({
-                        use: [{
-                            loader: "css-loader",
-                            options: {
-                                modules: true,
-                                localIdentName: '[name]__[local]___[hash:base64:5]',
-                                importLoaders: 2,
-                                sourceMap: true
-                            }
-                        }, {
-                            loader: "postcss-loader",
-                            options: {
-                                sourceMap: true
-                            }
-                        }, {
-                            loader: "sass-loader",
-                            options: {
-                                sourceMap: true
-                            }
-                        }],
-                        // use style-loader in development
-                        fallback: "style-loader"
-                    }),
-                },
-            ],
+  const isDevBuild = (env && env.dev) || (env && !env.prod) || true;
+
+  const cssProd = ExtractTextPlugin.extract({
+    use: [{
+      loader: 'css-loader',
+      options: {
+        modules: true,
+        localIdentName: '[name]__[local]___[hash:base64:5]',
+        camelCase: true,
+        importLoaders: 2,
+        sourceMap: false,
+      },
+    }, {
+      loader: 'postcss-loader',
+      options: {
+        sourceMap: false,
+      },
+    }, {
+      loader: 'sass-loader',
+      options: {
+        sourceMap: false,
+      },
+    }],
+  });
+
+  const cssDev = [
+    {
+      loader: 'style-loader',
+    },
+    {
+      loader: 'css-loader',
+      options: {
+        modules: true,
+        camelCase: true,
+        importLoaders: 2,
+        localIdentName: '[name]__[local]___[hash:base64:5]',
+        sourceMap: true,
+      },
+    },
+    {
+      loader: 'postcss-loader',
+      options: {
+        sourceMap: true,
+      },
+    },
+    {
+      loader: 'sass-loader',
+      options: {
+        sourceMap: true,
+      },
+    },
+  ];
+
+  return {
+    entry: { main: ['babel-polyfill', './ClientApp/boot.jsx'] },
+    devtool: 'inline-source-map',
+    resolve: { extensions: ['.js', '.jsx'] },
+    node: {
+      fs: 'empty',
+      net: 'empty',
+      tls: 'empty',
+      child_process: 'empty',
+    },
+    module: {
+      rules: [
+        {
+          enforce: 'pre',
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: 'eslint-loader',
+          options: {
+            fix: true,
+            emitWarning: true,
+          },
         },
-        plugins: [
-            new webpack.DllReferencePlugin({
-                context: __dirname,
-                manifest: require('./wwwroot/dist/vendor-manifest.json'), // eslint-disable-line global-require
-            }),
-            new ExtractTextPlugin('site.css'),
-        ],
-        output: {
-            path: path.resolve(__dirname, 'wwwroot', 'dist'),
-            filename: '[name].js',
-            publicPath: 'dist/',
+        {
+          test: /\.(js|jsx)$/,
+          include: /ClientApp/,
+          exclude: /(node_modules|bower_components)/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                babelrc: false,
+                presets: ['env', 'react'],
+                plugins: ['transform-class-properties', 'transform-es2015-destructuring', 'transform-object-rest-spread', 'transform-async-to-generator'],
+              },
+            },
+          ],
         },
-    };
+        {
+          test: /\.scss$/,
+          include: /ClientApp/,
+          use: isDevBuild ? cssDev : cssProd,
+        }, {
+          test: /\.(png|jpg|jpeg|gif|svg)$/,
+          include: /ClientApp/,
+          use: 'url-loader?limit=25000',
+        },
+      ],
+    },
+    plugins: [
+      new webpack.DllReferencePlugin({
+        context: __dirname,
+        manifest: require('./wwwroot/dist/vendor-manifest.json'), // eslint-disable-line global-require
+      }),
+      new ExtractTextPlugin('site.css'),
+    ],
+    output: {
+      path: path.resolve(__dirname, 'wwwroot', 'dist'),
+      filename: '[name].js',
+      publicPath: 'dist/',
+    },
+  };
 };
