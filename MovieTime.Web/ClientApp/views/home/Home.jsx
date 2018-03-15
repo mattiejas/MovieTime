528356d34cc1249c9ae195c348dc1608490783fd
Matthias Aarnoutse <mattiejas@outlook.com>
2018-03-15 23:30:54 +0100

    Clean-up navigation menu

--- MovieTime.Web/ClientApp/views/home/Home.jsx
+++ MovieTime.Web/ClientApp/views/home/Home.jsx
@@ -1,17 +1,9 @@
 import React from 'react';
-import Button from '../../components/button/Button';
-import { removeUser } from '../../utils/auth';
-
 
 export default class Home extends React.Component {
-  async handleClick(event) {
-    event.preventDefault();
-    const response = await removeUser('devopsproj');
-  }
-
   render() {
     return (
-      <Button onClick={this.handleClick} dark>Remove Me From Your System</Button>
+      <h1>Welcome to MovieTime!</h1>
     );
   }
 }
