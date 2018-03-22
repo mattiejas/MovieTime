42f89073300ca3a6df76328510bff3cd53ad6619
Hermen Otter <hermenotter@outlook.com>
2018-03-22 10:46:49 +0100

    Add movie poster caching to the backend

--- MovieTime.Web/Movies/MovieService.cs
+++ MovieTime.Web/Movies/MovieService.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.IO;
+using System.Net.Http;
 using System.Threading.Tasks;
-using AutoMapper;
+using Microsoft.AspNetCore.Hosting;
 using MovieTime.Web.Movies.Models;
 using MovieTime.Web.ThirdPartyServices;
 using MovieTime.Web.ThirdPartyServices.OMDB.MovieList;
+using Serilog;
 
 namespace MovieTime.Web.Movies
 {
@@ -11,11 +14,14 @@ namespace MovieTime.Web.Movies
     {
         private readonly IThirdPartyMovieRepository _thirdPartyMovieRepository;
         private readonly IMovieRespository _movieRespository;
+        private readonly IHostingEnvironment _hostingEnvironment;
 
-        public MovieService(IThirdPartyMovieRepository thirdPartyMovieRepository, IMovieRespository databaseMovieRespository)
+        public MovieService(IThirdPartyMovieRepository thirdPartyMovieRepository,
+            IMovieRespository databaseMovieRespository, IHostingEnvironment hostingEnvironment)
         {
             _thirdPartyMovieRepository = thirdPartyMovieRepository;
             _movieRespository = databaseMovieRespository;
+            _hostingEnvironment = hostingEnvironment;
         }
 
         /**
@@ -28,7 +34,7 @@ namespace MovieTime.Web.Movies
             var movieModel = await _movieRespository.Find(x => x.Id == id);
             if (movieModel != null) return movieModel;
 
-             movieModel = await _thirdPartyMovieRepository.GetMovieById(id);
+            movieModel = await _thirdPartyMovieRepository.GetMovieById(id);
             await AddMovie(movieModel); // Cache the movie in our database to improve robustness. Todo: temporary
 
             return movieModel;
@@ -39,7 +45,7 @@ namespace MovieTime.Web.Movies
             var movieModel = await _movieRespository.Find(x => x.Title.ToLower() == title.ToLower());
 
             if (movieModel != null) return movieModel;
-            
+
             movieModel = await _thirdPartyMovieRepository.GetMovieByTitle(title);
             await AddMovie(movieModel); // Cache the movie in our database to improve robustness. Todo: temporary
 
@@ -53,8 +59,50 @@ namespace MovieTime.Web.Movies
 
         public async Task<bool> AddMovie(Movie movie)
         {
+            if (movie == null) return false;
+
             var createdMovie = await _movieRespository.Add(movie);
+
+            if (!string.IsNullOrWhiteSpace(movie.Poster)) 
+                await DownloadMoviePoster(movie);
+            else
+                Log.Information($"Invalid poster information for {movie.Id} - {movie.Title}");
             return createdMovie != null;
+        }
+
+        private async Task DownloadMoviePoster(Movie movie)
+        {
+            var folderPath = Path.Combine(_hostingEnvironment.WebRootPath, "assets\\posters");
+            var fileName = movie.Id + Path.GetExtension(movie.Poster);
+            var fileUploadPath = Path.Combine(folderPath, fileName);
+            if (File.Exists(fileUploadPath))
+            {
+                Log.Information($"Poster {fileUploadPath} already exists in the assets folder.");
+            }
+            else
+            {
+                try
+                {
+                    Directory.CreateDirectory(folderPath);
+                    using (var httpClient = new HttpClient())
+                    using (var contentStream = await httpClient.GetStreamAsync(movie.Poster))
+                    {
+                        int bufferSize = 1048576;
+                        using (var fileStream = new FileStream(fileUploadPath, FileMode.Create, FileAccess.Write,
+                            FileShare.None, bufferSize, true))
+                        {
+                            await contentStream.CopyToAsync(fileStream);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log.Error(ex.Message);
+                }
+
+                if (File.Exists(fileUploadPath))
+                    Log.Information($"Poster {fileUploadPath} is succesfully retrieved to the assets folder");
+            }
         }
 
         public async Task<bool> MovieExistById(string movieId)
