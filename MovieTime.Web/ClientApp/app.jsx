4938f2c3356196f7eda19331726c4b3799c0bdd0
Hermen Otter <hermenotter@outlook.com>
2018-03-26 14:45:57 +0200

    Use redux-devtools-extension

--- MovieTime.Web/ClientApp/app.jsx
+++ MovieTime.Web/ClientApp/app.jsx
@@ -3,6 +3,7 @@ import thunk from 'redux-thunk';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import { applyMiddleware, combineReducers, createStore, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { ConnectedRouter, routerMiddleware, routerReducer } from 'react-router-redux';
 
 import Routes from './routes';
@@ -10,14 +11,14 @@ import history from './utils/history';
 import reducers from './modules';
 
 // eslint-disable-next-line no-underscore-dangle
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const middleware = routerMiddleware(history);
 const store = createStore(
   combineReducers({
     ...reducers,
     router: routerReducer,
   }),
-  composeEnhancers(applyMiddleware(thunk, middleware)),
+  composeWithDevTools(applyMiddleware(thunk, middleware)),
 );
 
 const App = ({ baseUrl }) => (
