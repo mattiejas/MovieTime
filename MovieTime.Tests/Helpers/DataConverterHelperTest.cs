8b1f12e93a84a1983ba3224c78e50899218beceb
Unknown <15049051@student.hhs.nl>
2018-03-26 14:32:35 +0200

    Add Xunit test with actual test

--- MovieTime.Tests/Helpers/DataConverterHelperTest.cs
+++ MovieTime.Tests/Helpers/DataConverterHelperTest.cs
@@ -1,7 +1,22 @@
-﻿namespace MovieTime.Tests.Helpers
+﻿using System;
+using FluentAssertions;
+using MovieTime.Web.Helpers;
+using Xunit;
+
+namespace MovieTime.Tests.Helpers
 {
     public class DataConverterHelperTest
     {
-        
+        [Theory]
+        [InlineData("123 min", 123)]
+        [InlineData("", null)]
+        [InlineData("532 ", 532)]
+        [InlineData("min", null)]
+        [InlineData("280", 280)]
+        public void TestOmdbRunTimeConverterWithCharacters(string runTime, int? expectedResult)
+        {
+            var result = DataConverterHelper.ConvertOmdbRuntimeToInt(runTime);
+            result.Should().Be(expectedResult);
+        }
     }
 }
