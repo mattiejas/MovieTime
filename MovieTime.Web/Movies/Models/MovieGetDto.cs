774a5dab18b4b7689348d12d2fb9a890b1fca303
Unknown <15049051@student.hhs.nl>
2018-04-03 01:24:05 +0200

    Map ImdbRating with database rating

--- MovieTime.Web/Movies/Models/MovieGetDto.cs
+++ MovieTime.Web/Movies/Models/MovieGetDto.cs
@@ -4,7 +4,7 @@
     {
         public string Title { get; set; }
         public string Year { get; set; }
-        public string Rated { get; set; }
+        public string ImdbRating { get; set; }
         public string Poster { get; set; }
         public int RunTime { get; set; }
         public string Genre { get; set; }
